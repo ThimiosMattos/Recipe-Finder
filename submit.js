e4e8a5f8224b22a7a45f4b9614b9105b26efa782
Thimios Mattos <efthymiosmattos@gmail.com>
2025-01-13 14:21:07 +0100

    Revert "test"
    This reverts commit 45d12fc885e341c915f1d8d97971aefa88f8cc1e.

--- submit.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('recipe-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    document.getElementById('message').innerText = 'Ditt recept har skickats in!';
-
-    document.getElementById('recipe-form').reset();
-});
